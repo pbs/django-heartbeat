e23f13b2f88e82a9c9daaa46c348354a57cf400e
Dan Claudiu Pop <danclaudiupop@gmail.com>
2016-02-08 17:57:06 +0200

    auth by whitelisted ips

--- src/heartbeat/auth.py
+++ src/heartbeat/auth.py
@@ -1,3 +1,4 @@
+import re
 import base64
 
 from functools import wraps
@@ -7,10 +8,16 @@ from django.http import HttpResponse
 from django.core.exceptions import ImproperlyConfigured
 
 
-def http_basic_auth(func):
+def auth(func):
     @wraps(func)
     def _decorator(request, *args, **kwargs):
-        prepare_auth()
+        auth = get_auth()
+        if 'authorized_ips' in auth:
+            ip = get_client_ip(request)
+            if ip in auth['authorized_ips']:
+                return func(request, *args, **kwargs)
+
+        prepare_credentials(auth)
         if request.META.get('HTTP_AUTHORIZATION'):
             authmeth, auth = request.META['HTTP_AUTHORIZATION'].split(' ')
             if authmeth.lower() == 'basic':
@@ -19,6 +26,7 @@ def http_basic_auth(func):
                 if (username == HEARTBEAT['auth']['username'] and
                         password == HEARTBEAT['auth']['password']):
                     return func(request, *args, **kwargs)
+
         response = HttpResponse(
             "your not wise enough to be in PonYLaNd 1337", status=401)
         response['WWW-Authenticate'] = 'Basic realm="Welcome to PonYLaNd 1337"'
@@ -26,12 +34,48 @@ def http_basic_auth(func):
     return _decorator
 
 
-def prepare_auth():
+def get_auth():
     auth = HEARTBEAT.get('auth')
     if not auth:
         raise ImproperlyConfigured('Missing auth configuration for heartbeat')
+    return auth
 
+
+def prepare_credentials(auth):
     if not all([auth.get('username'), auth.get('password')]):
         raise ImproperlyConfigured(
             'Username or password missing from auth configuration '
             'for heartbeat')
+
+
+def get_access_route(request):
+    meta = request.META
+    return (
+        meta.get('HTTP_X_FORWARDED_FOR') or meta.get('REMOTE_ADDR')
+    ).split(',')
+
+
+def get_client_ip(request):
+    access_route = get_access_route(request)
+
+    if len(access_route) == 1:
+        return access_route[0]
+    expression = """
+        (^(?!(?:[0-9]{1,3}\.){3}[0-9]{1,3}$).*$)|  # will match non valid ipV4
+        (^127\.0\.0\.1)|  # will match 127.0.0.1
+        (^10\.)|  # will match 10.0.0.0 - 10.255.255.255 IP-s
+        (^172\.1[6-9]\.)|  # will match 172.16.0.0 - 172.19.255.255 IP-s
+        (^172\.2[0-9]\.)|  # will match 172.20.0.0 - 172.29.255.255 IP-s
+        (^172\.3[0-1]\.)|  # will match 172.30.0.0 - 172.31.255.255 IP-s
+        (^192\.168\.)  # will match 192.168.0.0 - 192.168.255.255 IP-s
+    """
+    regex = re.compile(expression, re.X)
+    for ip in access_route:
+        if not ip:
+            # it's possible that the first value from X_FORWARDED_FOR
+            # will be null, so we need to pass that value
+            continue
+        if regex.search(ip):
+            continue
+        else:
+            return ip
