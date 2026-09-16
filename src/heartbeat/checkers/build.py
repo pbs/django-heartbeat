3710fcfcff7a9a01f0c4408e780c8a14454515eb
Lorand Varga <lorand.varga@3pillarglobal.com>
2026-09-16 12:13:15 +0300

    OCTO-11613 No module named 'pkg_resources' - django-heartbeat
    First pass.

--- src/heartbeat/checkers/build.py
+++ src/heartbeat/checkers/build.py
@@ -1,4 +1,4 @@
-from pkg_resources import Requirement, WorkingSet
+from importlib.metadata import PackageNotFoundError, distribution
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
@@ -10,11 +10,8 @@ def check(request):
         raise ImproperlyConfigured(
             'Missing package_name key from heartbeat configuration')
 
-    sys_path_distros = WorkingSet()
-    package_req = Requirement.parse(package_name)
-
-    distro = sys_path_distros.find(package_req)
-    if not distro:
+    try:
+        dist = distribution(package_name)
+        return dict(name=dist.metadata['Name'], version=dist.metadata['Version'])
+    except PackageNotFoundError:
         return dict(error='no distribution found for {}'.format(package_name))
-
-    return dict(name=distro.project_name, version=distro.version)
